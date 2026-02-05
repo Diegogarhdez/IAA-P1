bc88109b69cfa37a133c85256682f587fef2753e
Diegogarhdez <alu0101633732@ull.edu.es>
2026-02-05 13:54:59 +0000

    minor csv changes

--- csv_loader.py
+++ csv_loader.py
@@ -1,7 +1,6 @@
 import csv
 
 def load_data(fichero):
-
   with open(fichero, newline="", encoding="utf-8") as f:
     reader = csv.reader(f)
     distribucion = []
@@ -11,10 +10,8 @@ def load_data(fichero):
       raise RuntimeError("empty input file")
     tamaño_entrada = len(distribucion[0][0])
     distribucion_procesada = [0.0] * (2 ** tamaño_entrada)
-
     for fila in distribucion:
       indice = int(fila[0], 2)       
       valor = float(fila[1])         
       distribucion_procesada[indice] = valor
-
-    print(distribucion_procesada)
+  return distribucion_procesada

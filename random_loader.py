a585a8e77f9242a2cc9e1b4fbda221351af6ddf7
Kyrylo Chvanov <alu0101640440@ull.edu.es>
2026-02-05 13:29:51 +0000

    Add random loader

---
+++ random_loader.py
@@ -0,0 +1,11 @@
+import random
+import pprint
+
+def generate_random_distribution(number_random_variables: int) -> list[float]:
+  if number_random_variables <= 0:
+    raise ValueError("invalid number of variables")
+  distribution = [0.0] * (2 ** number_random_variables)
+  for i in range(2 ** number_random_variables):
+    distribution[i] = random.randint(1, 100)
+  normalized = [float(i)/sum(distribution) for i in distribution]
+  return normalized

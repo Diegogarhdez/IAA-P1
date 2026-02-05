774038937d39647a2e2dfd221178cbf041ad3c10
Kyrylo Chvanov <alu0101640440@ull.edu.es>
2026-02-05 14:11:27 +0000

    Replace raises with asserts

--- random_loader.py
+++ random_loader.py
@@ -2,8 +2,7 @@ import random
 from distribution import Distribution
 
 def generate_random_distribution(number_random_variables: int) -> list[float]:
-  if number_random_variables <= 0:
-    raise ValueError("invalid number of variables")
+  assert number_random_variables <= 0
   distribution = [0.0] * (2 ** number_random_variables)
   for i in range(2 ** number_random_variables):
     distribution[i] = random.randint(1, 100)
